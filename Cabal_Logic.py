b5e9f395569fdec3f736a63ec1027ba5bd9908ab
MrocznyZdzichu <maksymzawrotny@wp.pl>
2022-06-30 16:21:30 +0200

    CC2Alz calculation implemented

--- Cabal_Logic.py
+++ Cabal_Logic.py
@@ -2,3 +2,8 @@ def gem2alz(gems, voucher_price):
     gems_per_voucher = 250.0
     fraction_of_voucher = gems/gems_per_voucher
     return round(fraction_of_voucher*voucher_price, 3)
+
+def cc2alz(cc, voucher_price):
+    cc_per_voucher = 5000
+    fraction_of_voucher = cc/cc_per_voucher
+    return round (fraction_of_voucher*voucher_price, 3)
--- Window_Logic.py
+++ Window_Logic.py
@@ -5,7 +5,9 @@ from PySide2.QtCore import SIGNAL, QObject
 class GUI_Logic:
     def connect_signals(self):
         QObject.connect(self.window_handle.pb_compute,
-                        SIGNAL('clicked()'), self.__pb_compute_slot)
+                        SIGNAL('clicked()'), self.__pb_compute_gems_slot)
+        QObject.connect(self.window_handle.pb_compute_cc,
+                        SIGNAL('clicked()'), self.__pb_compute_cc_slot)
 
     def __init__(self, window):
         self.window_handle = window
@@ -33,10 +35,34 @@ class GUI_Logic:
     def __log_msg(self, msg):
         self.window_handle.tb_console.append(msg)
 
-    def __pb_compute_slot(self):
-        gems = int(self.__get_price_in_gems())
-        voucher_price = float(self.__get_voucher_price())
+    def __pb_compute_gems_slot(self):
+        gems = self.__get_price_in_gems()
+        voucher_price = self.__get_voucher_price()
 
         alz_equivalent = str(CL.gem2alz(gems, voucher_price))
         self.__log_msg(f'Gems calculated in alz: {alz_equivalent}')
         self.__set_alz_cost(alz_equivalent)
+
+    def __get_price_in_cc(self):
+        cc = self.__get_le_text(self.window_handle.le_cc)
+        self.__log_msg_cc(f'Cost in CC: {cc}')
+        return int(cc)
+
+    def __get_voucher_price_cc(self):
+        vp = self.__get_le_text(self.window_handle.le_vp)
+        self.__log_msg_cc(f'Cost of voucher in alz: {vp}')
+        return float(vp)
+
+    def __log_msg_cc(self, msg):
+        self.window_handle.tb_console_cc.append(msg)
+
+    def __set_alz_cost_cc(self, new_text):
+        self.__set_line_edit(self.window_handle.le_ro_cc2alz_res, new_text)
+
+    def __pb_compute_cc_slot(self):
+        cc = self.__get_price_in_cc()
+        vp = self.__get_voucher_price_cc()
+
+        alz_equivalent = str(CL.cc2alz(cc, vp))
+        self.__log_msg_cc(f'CC calculated in alz: {alz_equivalent}')
+        self.__set_alz_cost_cc(alz_equivalent)
