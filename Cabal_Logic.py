c0eb17d3fd98d358bd94387435ac8581dc2ddd35
MrocznyZdzichu <maksymzawrotny@wp.pl>
2022-06-30 15:39:51 +0200

    Improved verbosivity of gem calc. Add result rounding to 3 decimals. Getters and setters refactor. Minor layout improvement

--- Cabal_Logic.py
+++ Cabal_Logic.py
@@ -1,4 +1,4 @@
 def gem2alz(gems, voucher_price):
     gems_per_voucher = 250.0
     fraction_of_voucher = gems/gems_per_voucher
-    return fraction_of_voucher*voucher_price
+    return round(fraction_of_voucher*voucher_price, 3)
--- Window_Logic.py
+++ Window_Logic.py
@@ -5,30 +5,38 @@ from PySide2.QtCore import SIGNAL, QObject
 class GUI_Logic:
     def connect_signals(self):
         QObject.connect(self.window_handle.pb_compute,
-                        SIGNAL ('clicked()'), self.__pb_compute_slot)
-
+                        SIGNAL('clicked()'), self.__pb_compute_slot)
 
     def __init__(self, window):
         self.window_handle = window
         self.connect_signals()
 
+    def __get_le_text(self, le):
+        return le.text()
 
     def __get_price_in_gems(self):
-        return self.window_handle.le_gems.text()
-
+        gems = self.__get_le_text(self.window_handle.le_gems)
+        self.__log_msg(f'Cost in gems: {gems}')
+        return int(gems)
 
     def __get_voucher_price(self):
-        return self.window_handle.le_voucher_price.text()
+        vp = self.__get_le_text(self.window_handle.le_voucher_price)
+        self.__log_msg(f'Cost of voucher in alz: {vp}')
+        return float(vp)
 
+    def __set_line_edit(self, le, inText):
+        le.setText(inText)
 
     def __set_alz_cost(self, new_text):
-        self.window_handle.le_result.setText(new_text)
+        self.__set_line_edit(self.window_handle.le_result, new_text)
 
+    def __log_msg(self, msg):
+        self.window_handle.tb_console.append(msg)
 
     def __pb_compute_slot(self):
         gems = int(self.__get_price_in_gems())
         voucher_price = float(self.__get_voucher_price())
 
         alz_equivalent = str(CL.gem2alz(gems, voucher_price))
-
+        self.__log_msg(f'Gems calculated in alz: {alz_equivalent}')
         self.__set_alz_cost(alz_equivalent)
